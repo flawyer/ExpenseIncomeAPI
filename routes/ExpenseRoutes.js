294b7189482269b6dbc24e54e46d07d94268511b
Flawyer <Nimadongtamang1@gmail.com>
2024-07-12 21:57:29 +0545

    Expense

--- routes/ExpenseRoutes.js
+++ routes/ExpenseRoutes.js
@@ -6,6 +6,7 @@ const Expense = require('../controllers/ExpenseController');
 router.get('/MonthlySaving/:id', Expense.GetExpense);
 router.put('/Expense/:id', Expense.UpdateExpense);
 router.get('/Expense', Expense.GetAllExpense);
+router.post('/Expense',Expense.InsertExpense);
 router.delete('/Expense/:id', Expense.DeleteExpense);
 
 module.exports =  router ; 
